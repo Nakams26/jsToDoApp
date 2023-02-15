996c263a7d0f08893b7feb25736ced016009da5a
adrienpolselli <adrienpolselli@hotmail.com>
2023-02-15 15:44:03 -0500

    Adding Pseudo code

---
+++ script.js
@@ -0,0 +1,23 @@
+//Pseudo code
+
+
+//1 Save the value
+//Let's query the dom the form element and save it as a variable
+// Let's query the dom for the input
+
+
+
+//2 Attach a submit event listener to the form element
+ //Pass in 2 argument the event listener method
+   //a) the event you are listing for
+   //b)The callback fucntion which will hold the logic we wish to run once the event is heard (AKA occurs on the page)
+
+//Save the text value from the input within a variable
+
+   //  if the user submit an item (check wether the value of the input is not empty), then do some thing:
+      // Create a li element
+        // a font awesome to it
+        // add the text from the item to the li
+        
+        //Append the li element to the ul element 
+   //    else alert the user to submit a valid data
